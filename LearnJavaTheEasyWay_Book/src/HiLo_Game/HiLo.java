69d1011c628b9a6f37b2233bb22bf995371f662a
DylanPerkins <dp011400@gmail.com>
2023-03-08 00:37:40 -0500

    Added continue to play feature + closed resc. leak

--- LearnJavaTheEasyWay_Book/src/HiLo_Game/HiLo.java
+++ LearnJavaTheEasyWay_Book/src/HiLo_Game/HiLo.java
@@ -5,27 +5,36 @@ import java.util.Scanner;
 public class HiLo {
     public static void main(String[] args) {
 
-        Scanner scan = new Scanner(System.in);      
+        Scanner scan = new Scanner(System.in);
 
-        // Create a random number for the user to guess
-        int theNumber = (int)(Math.random() * 100 + 1);
-        int userGuess = 0;
+        String playAgain = "";
 
-        
-        while (userGuess != theNumber) {
-        // Asks user to make their input
-            System.out.println("Guess a number between 1 and 100:");
-            userGuess = scan.nextInt();
+        do {
+            // Create a random number for the user to guess and initilize a variable to hold their guess.
+            int theNumber = (int) (Math.random() * 100 + 1);
+            int userGuess = 0;
 
-            if (userGuess < theNumber) {
-                System.out.println(userGuess + " is too low. Please try again!");
-            } else if (userGuess > theNumber) {
-                System.out.println(userGuess + " is too high. Please try again!");
-            } else if (userGuess == theNumber) {
-                System.out.println(userGuess + " was correct! Nice job!");
-            } else {
-                System.out.println("Silly goose, this number is invalid!");
-            }
-        }
+            while (userGuess != theNumber) {
+                // Asks user to make their input, then stores that input in userGuess.
+                System.out.println("Guess a number between 1 and 100:");
+                userGuess = scan.nextInt();
+
+                if (userGuess < theNumber) {
+                    System.out.println(userGuess + " is too low. Please try again!");
+                } else if (userGuess > theNumber) {
+                    System.out.println(userGuess + " is too high. Please try again!");
+                } else if (userGuess == theNumber) {
+                    System.out.println(userGuess + " was correct! Nice job!");
+                } else {
+                    System.out.println("Silly goose, this input is invalid!");
+                }
+            } // End of guessing loop.
+
+            System.out.println("Would you like to play again? (y/n)");
+            playAgain = scan.next();
+        } while (playAgain.equalsIgnoreCase("y")); // End of do-while loop.
+
+        System.out.println("Thank you for playing my game! Byee!");
+        scan.close(); // Closes the scanner system, preventing resource leaks.
     }
 }
