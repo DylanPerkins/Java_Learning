742c434e0c7fbc57a3d9a48fc5df2c5556885742
DylanPerkins <dp011400@gmail.com>
2023-03-07 23:26:22 -0500

    Re-did file structure, added some more to HiLo

--- LearnJavaTheEasyWay_Book/src/HiLo_Game/HiLo.java
+++ LearnJavaTheEasyWay_Book/src/HiLo_Game/HiLo.java
@@ -1,19 +1,19 @@
-package Chapter_2.HiLo_Game;
+package HiLo_Game;
 
 import java.util.Scanner;
 
 public class HiLo {
     public static void main(String[] args) {
 
-        Scanner scan = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);      
 
         // Create a random number for the user to guess
         int theNumber = (int)(Math.random() * 100 + 1);
 
         int userGuess;
 
-        System.out.println(theNumber);
-
-
+        // Asks user to make their input
+        System.out.println("Guess a number between 1 and 100:");
+        userGuess = scan.nextInt();
     }
 }
