3c9b878a4053661e59fffbeb3adedc64a53ea8a3
DylanPerkins <dp011400@gmail.com>
2023-03-07 23:09:57 -0500

    Initial Push.

---
+++ LearnJavaTheEasyWay_Book/src/Chapter_2/HiLo_Game/HiLo.java
@@ -0,0 +1,19 @@
+package Chapter_2.HiLo_Game;
+
+import java.util.Scanner;
+
+public class HiLo {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        // Create a random number for the user to guess
+        int theNumber = (int)(Math.random() * 100 + 1);
+
+        int userGuess;
+
+        System.out.println(theNumber);
+
+
+    }
+}
