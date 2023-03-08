31eaf88e47e5d0fb529cce34bb884c02a5c08d58
DylanPerkins <dp011400@gmail.com>
2023-03-07 23:54:51 -0500

    Game works in it's most basic form!

--- LearnJavaTheEasyWay_Book/src/HiLo_Game/HiLo.java
+++ LearnJavaTheEasyWay_Book/src/HiLo_Game/HiLo.java
@@ -9,11 +9,23 @@ public class HiLo {
 
         // Create a random number for the user to guess
         int theNumber = (int)(Math.random() * 100 + 1);
+        int userGuess = 0;
 
-        int userGuess;
-
+        
+        while (userGuess != theNumber) {
         // Asks user to make their input
-        System.out.println("Guess a number between 1 and 100:");
-        userGuess = scan.nextInt();
+            System.out.println("Guess a number between 1 and 100:");
+            userGuess = scan.nextInt();
+
+            if (userGuess < theNumber) {
+                System.out.println(userGuess + " is too low. Please try again!");
+            } else if (userGuess > theNumber) {
+                System.out.println(userGuess + " is too high. Please try again!");
+            } else if (userGuess == theNumber) {
+                System.out.println(userGuess + " was correct! Nice job!");
+            } else {
+                System.out.println("Silly goose, this number is invalid!");
+            }
+        }
     }
 }
